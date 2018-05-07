cdc744818a8ac935f216f0f371368bbdb56ce7e9
Euan <bell_designs@outlook.com>
2018-05-07 12:00:11 +0100

    complete self.all on Country claas

--- models/country.rb
+++ models/country.rb
@@ -5,25 +5,32 @@ class Country
 
   attr_reader :id, :country_name
 
-def initialize(options)
-  @id = options['id'].to_i if options['id']
-  @country_name = options['country_name']
-end
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @country_name = options['country_name']
+  end
 
-def save()
-  sql = "INSERT INTO countries
-  (
-    country_name
-  )
-  VALUES
-  (
-    $1
-  )
-  RETURNING id"
-  values = [@country_name]
-  country = SqlRunner.run( sql, values ).first
-  @id = country['id'].to_i
-end
+  def save()
+    sql = "INSERT INTO countries
+    (
+      country_name
+    )
+    VALUES
+    (
+      $1
+    )
+    RETURNING id"
+    values = [@country_name]
+    country = SqlRunner.run( sql, values ).first
+    @id = country['id'].to_i
+  end
 
+  def self.find( id )
+    sql = "SELECT * FROM countries
+    WHERE id = $1"
+    values = [id]
+    results = SqlRunner.run( sql, values )
+    return Country.new( results.first )
+  end
 
 end
