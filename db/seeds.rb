9633b93146de15b2cbde4b7cac8174ae1ab7c739
Euan <bell_designs@outlook.com>
2018-05-09 11:24:20 +0100

    fully populated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -74,6 +74,42 @@ location3 = Location.new({
   })
 location3.save()
 
+location4 = Location.new({
+    'location_name' => 'Bifrost the Rainbow Bridge',
+    'realm_id' => realm4.id
+  })
+location4.save()
+
+location5 = Location.new({
+    'location_name' => 'Mimir’s well of wisdom',
+    'realm_id' => realm5.id
+  })
+location5.save()
+
+location6 = Location.new({
+    'location_name' => 'The seeing lakes of Vanaheim',
+    'realm_id' => realm6.id
+  })
+location6.save()
+
+location7 = Location.new({
+    'location_name' => 'Alfheim Sanctuary',
+    'realm_id' => realm7.id
+  })
+location7.save()
+
+location8 = Location.new({
+    'location_name' => 'The Great Forge',
+    'realm_id' => realm8.id
+  })
+location8.save()
+
+location9 = Location.new({
+    'location_name' => 'The Hellway',
+    'realm_id' => realm9.id
+  })
+location9.save()
+
 visit1 = Visit.new({
     'location_id' => location1.id,
     'realm_id' => realm1.id
@@ -91,3 +127,40 @@ visit2 = Visit.new({
     'realm_id' => realm3.id
     })
       visit3.save()
+
+      visit4 = Visit.new({
+        'location_id' => location4.id,
+        'realm_id' => realm4.id
+        })
+  visit4.save()
+
+  visit5 = Visit.new({
+  'location_id' => location5.id,
+  'realm_id' => realm5.id
+  })
+  visit5.save()
+
+
+  visit6 = Visit.new({
+  'location_id' => location6.id,
+  'realm_id' => realm6.id
+  })
+  visit6.save()
+
+  visit7 = Visit.new({
+  'location_id' => location7.id,
+  'realm_id' => realm7.id
+  })
+  visit7.save()
+
+  visit8 = Visit.new({
+  'location_id' => location8.id,
+  'realm_id' => realm8.id
+  })
+  visit8.save()
+
+  visit9 = Visit.new({
+  'location_id' => location9.id,
+  'realm_id' => realm9.id
+  })
+  visit9.save()
